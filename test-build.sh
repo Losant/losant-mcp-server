42fbc5dceb6ada5e0f4b6bd12e57200e6abf6c72
Erin Thompson <erin@losant.com>
2026-06-10 12:03:26 -0400

    remove stuff

--- test-build.sh
+++
@@ -1,8 +0,0 @@
-set -euo pipefail
-IFS=$'\n\t'
-echo $BRANCH_NAME
-tagName=${TAG_NAME:-$BRANCH_NAME}
-echo $tagName
-tag="us-docker.pkg.dev/${PROJECT_ID}/${_ORG_NAME}/${_IMAGE_NAME}:${tagName}"
-echo $tag
-docker buildx build --attest=type=sbom,mode=max --tag=$tag --file=./Dockerfile . 
